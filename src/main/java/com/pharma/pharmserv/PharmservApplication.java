91f3efdade64c484e76415d05f89aeb3e5c30d1b
RinzlerN26 <>
2025-01-18 17:47:14 +0530

    Initial Commit.

---
+++ src/main/java/com/pharma/pharmserv/PharmservApplication.java
@@ -0,0 +1,13 @@
+package com.pharma.pharmserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharma/pharmserv/PharmservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharma.pharmserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
