d063b7649b04f48a7754e3da4086aea9491ccb7c
RinzlerN26 <>
2025-01-25 19:53:14 +0530

    Added Suppress warning.

--- src/main/java/com/pharma/pharmserv/UserRepository.java
+++ src/main/java/com/pharma/pharmserv/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.pharma.pharmserv.User;
 
+@SuppressWarnings("unused")
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
