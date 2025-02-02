c9da182ead731434850efd3f5a9a32595301432d
RinzlerN26 <>
2025-02-02 15:35:20 +0530

    Cascade Changes.

--- src/main/java/com/pharma/pharmserv/Entities/Pharma.java
+++ src/main/java/com/pharma/pharmserv/Entities/Pharma.java
@@ -7,7 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.CascadeType;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 public class Pharma {
@@ -21,8 +25,9 @@ public class Pharma {
     private String dealerName;
     private LocalDate expiryDate;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     public String getMedicineName() {
