179e3033f4ac71e387396369ab869b77074ef600
RinzlerN26 <nishantonnet45@gmail.com>
2025-12-24 12:45:31 +0530

    Updated Security and Properties

--- src/main/java/com/pharma/pharmserv/SecurityConfig.java
+++ src/main/java/com/pharma/pharmserv/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.pharma.pharmserv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
