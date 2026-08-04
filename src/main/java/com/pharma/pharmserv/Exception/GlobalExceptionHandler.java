0bfb449d174052296c992bc2ccac040dc73d2202
RinzlerN26 <nishantonnet45@gmail.com>
2026-08-04 22:58:21 +0530

    Add Global Exception Handler.

---
+++ src/main/java/com/pharma/pharmserv/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.pharma.pharmserv.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.pharma.pharmserv.DTO.ErrorDto;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                new ErrorDto(
+                        LocalDateTime.now(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        ex.getMessage()));
+    }
+}
